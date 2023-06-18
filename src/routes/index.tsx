cb476bed5b7c09f5c575324f3d2ae0994ed0cefe
Josh Harwood <josh@noworries.nz>
2023-06-19 11:24:39 +1200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <main>
       <Title>DS doc search</Title>
-      <h1>Search design system documentation</h1>
+      <h1>Design System Search</h1>
+      <p>Easily search public design system documentation.</p>
       <script async src="https://cse.google.com/cse.js?cx=927b3548930934c92">
 </script>
 <div class="gcse-search"></div>
